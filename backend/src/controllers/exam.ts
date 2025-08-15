bbf65468356097e48d0707a5d0cc58dbf87fe6c1
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-15 19:44:00 +0530

    Added attempted question increment and passed count increment tasks to subitExam function

--- backend/src/controllers/exam.ts
+++ backend/src/controllers/exam.ts
@@ -3,6 +3,7 @@ import ProjectError from "../helper/ProjectError";
 import Quiz from "../models/quiz";
 import Report from "../models/report";
 import { ReturnResponse } from "../util/interfaces";
+import User from "../models/user";
 
 const startExam = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -30,12 +31,13 @@ const submitExam = async (req: Request, res: Response, next: NextFunction) => {
         const attempted_questions = req.body.attempted_questions;
         const quizId = req.body.quizId;
 
-        const quiz = await Quiz.findById(quizId, { answers: 1, created_by: 1 });
+        const quiz = await Quiz.findById(quizId, { answers: 1, created_by: 1, passing_percentage: 1 });
         const answers = quiz?.answers;
+        const passingPercentage = quiz?.passing_percentage || 25;
         const userId = req.userId;
         const allQuestions = Object.keys(answers);
         const total = allQuestions.length;
-        
+
         if (userId === quiz?.created_by.toString()) {
             const err = new ProjectError("You cannot submit your own quiz.");
             err.statusCode = 405;
@@ -48,9 +50,26 @@ const submitExam = async (req: Request, res: Response, next: NextFunction) => {
                 score++;
             }
         }
-        const report = new Report({ userId, quizId, score, total });
+
+        const percentage = (score / total) * 100;
+        let result = "";
+        if (percentage >= passingPercentage) {
+            result = "Pass";
+        } else {
+            result = "Fail";
+        }
+
+        const report = new Report({ userId, quizId, score, total, percentage, result });
         const data = await report.save();
-        const resp: ReturnResponse = { status: "success", message: "Quiz submitted", data: { total, score, reportId: data._id } }
+        const user = await User.findById(userId, { attemptedQuizCount: 1 });
+        if (user) {
+            user.attemptedQuizCount += 1;
+            if (result === "Pass") {
+                user.passedCount += 1;
+            }
+            await user.save();
+        }
+        const resp: ReturnResponse = { status: "success", message: "Quiz submitted", data: { total, score, percentage, result, reportId: data._id } }
         res.status(200).send(resp);
     } catch (error) {
         next(error);
