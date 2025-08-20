e006a167126c3ae67ae9e3575fc7f3368fe3a512
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-20 16:56:26 +0530

    Added a function to register a user via fetch request to the backend

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,26 +1,87 @@
-import { Link } from "react-router-dom"
-import AuthInput from "../components/AuthInput"
-import Button from "../components/Button"
-import { MdMailOutline, MdPersonOutline } from "react-icons/md"
-import { HiOutlineLockClosed } from "react-icons/hi";
-import { BsShieldLock } from "react-icons/bs";
-import logo from "/logo.png";
+import { useRef } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import AuthInput from '../components/AuthInput'
+import Button from '../components/Button'
+import { MdMailOutline, MdPersonOutline } from 'react-icons/md'
+import { HiOutlineLockClosed } from 'react-icons/hi'
+import { BsShieldLock } from 'react-icons/bs'
+import logo from '/logo.png'
+
+const serverURL = "http://localhost:3000";
 
 const Register = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passRef = useRef<HTMLInputElement>(null);
+  const cpassRef = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
+  function validateUser(userName: string, email: string, pass: string, cpass: string): Boolean {
+    let valid = true;
+    if (userName.length < 4) {
+      alert("Name should contain at least 4 characters.");
+      valid = false;
+    } else if (email.length < 5) {
+      alert("Email should contain at least 6 characters with an '@' symbol in it.")
+    } else if (pass.length < 8) {
+      alert("Password should be 8 characters long.");
+      valid = false;
+    } else if (cpass !== pass) {
+      alert("Confirm password should be same as Password");
+      valid = false;
+    }
+    return valid;
+  }
+
+  async function registerUser(event: React.FormEvent) {
+    event.preventDefault();
+    const userName = "" + nameRef.current?.value;
+    const email = "" + emailRef.current?.value;
+    const pass = "" + passRef.current?.value;
+    const cpass = "" + cpassRef.current?.value
+    if (!validateUser(userName, email, pass, cpass)) {
+      return;
+    }
+    const response = await fetch(serverURL + "/auth", {
+      method: 'POST',
+      body: JSON.stringify({
+        name: userName,
+        email: email,
+        password: pass,
+        confirm_password: cpass
+      }),
+      headers: {
+        "content-type": "application/json"
+      }
+    })
+    const data = await response.json();
+    if (data.status === "success") {
+      alert(data.message);
+      navigate('/login');
+    }
+    else {
+      alert(data.message);
+    }
+  }
+
   return (
-    <div className="w-4/5 h-full flex flex-col justify-around items-center">
-      <div className="flex items-center [&_img]:m-2">
-        <img src={logo} alt="quizriousity-logo" width={95} height={95} />
-        <p className="text-6xl">Register</p>
+    <div className='w-4/5 h-full flex flex-col justify-around items-center'>
+      <div className='flex items-center [&_img]:m-2'>
+        <img src={logo} alt='quizriousity-logo' width={95} height={95} />
+        <p className='text-6xl'>Register</p>
       </div>
-      <div className="grid gap-10 w-2/5 text-2xl *:flex *:flex-col [&_input]:rounded-full [&_input]:bg-[#7dcdff] [&_input]:h-10 [&_input]:px-3.5 [&_input]:outline-none [&_input]:border-none">
-        <AuthInput id={"username"} icon={MdPersonOutline} type="text" label="Name" />
-        <AuthInput id={"e-mail"} icon={MdMailOutline} type="email" label="E-mail" />
-        <AuthInput id={"pass"} icon={HiOutlineLockClosed} type="password" label="Password" />
-        <AuthInput id={"cpass"} icon={BsShieldLock} type="password" label="Confirm Password" />
-      </div>
-      <p className="text-xl">Already have an account? <Link className="text-blue-700 underline" to="/login">Login</Link></p>
-      <Button value="Submit" onClick={() => { console.log("Register submitted") }} />
+      <form className='w-full flex flex-col justify-between items-center' onSubmit={(e) => registerUser(e)}>
+        <div className='grid gap-10 w-2/5 text-2xl *:flex *:flex-col [&_input]:rounded-full [&_input]:bg-[#7dcdff] [&_input]:h-10 [&_input]:px-3.5 [&_input]:outline-none [&_input]:border-none'>
+
+          <AuthInput id={'username'} icon={MdPersonOutline} type='text' label='Name' ref={nameRef} />
+          <AuthInput id={'e-mail'} icon={MdMailOutline} type='email' label='E-mail' ref={emailRef} />
+          <AuthInput id={'pass'} icon={HiOutlineLockClosed} type='password' label='Password' ref={passRef} />
+          <AuthInput id={'cpass'} icon={BsShieldLock} type='password' label='Confirm Password' ref={cpassRef} />
+        </div>
+        <p className='text-xl'>Already have an account? <Link className='text-blue-700 underline' to='/login'>Login</Link></p>
+        <div className='grid place-items-center m-5'>
+          <Button value='Submit' onClick={null} />
+        </div>
+      </form>
     </div>
   )
 }
