869725305bd39f1d3e13d5062423ca6f93988fa6
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-20 18:08:12 +0530

    Created quizzes page with a navbar and search bar

---
+++ frontend/src/pages/Quizzes.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar"
+import SearchBar from "../components/SearchBar"
+
+const Quizzes = () => {
+  return (
+    <div className="w-full h-full">
+      <Navbar/>
+      <SearchBar id="quiz"/>
+      <hr />
+    </div>
+  )
+}
+
+export default Quizzes
