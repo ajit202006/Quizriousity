a066678a241d4040e3bbe81350ff23e12db0375d
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-20 10:47:11 +0530

    Added routes for register and login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
 
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+function App() {
   return (
-    <>
-    </>
+    <div className="h-screen w-screen flex items-center justify-center bg-[#429BB7] text-white">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+
   )
 }
 
