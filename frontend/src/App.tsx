2a54c53f5ccbf8039f54f664e4b2f6692b31ab92
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-20 18:02:01 +0530

    Added route to reports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 
-import Home from "./pages/Home";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Quizzes from './pages/Quizzes';
+import Reports from './pages/Reports';
 
 function App() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center bg-[#429BB7] text-white">
+    <div className='h-screen w-screen flex items-center justify-center bg-[#429BB7] text-white'>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/quizzes' element={<Quizzes />} />
+        <Route path='/reports' element={<Reports />} />
+
       </Routes>
     </div>
 
