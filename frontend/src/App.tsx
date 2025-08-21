1159e78f55e10ef224d7e34825d5fd83770f5bf2
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-21 15:21:37 +0530

    Added route for users tab

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,20 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import Quizzes from './pages/Quizzes';
 import Reports from './pages/Reports';
+import Users from './pages/Users';
 
 function App() {
   return (
-    <div className='h-screen w-screen flex items-center justify-center bg-[#429BB7] text-white'>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/quizzes' element={<Quizzes />} />
-        <Route path='/reports' element={<Reports />} />
-
-      </Routes>
-    </div>
-
+      <div className='h-screen w-screen flex items-center justify-center bg-[#429BB7] text-white'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/quizzes' element={<Quizzes />} />
+          <Route path='/reports' element={<Reports />} />
+          <Route path='/users' element={<Users />} />
+        </Routes>
+      </div>
   )
 }
 
