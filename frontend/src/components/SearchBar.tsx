346f9d63e2a716502718013344d0e2992759b979
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-21 17:55:59 +0530

    Added click event on search button

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,14 +1,20 @@
 import { IoSearch } from 'react-icons/io5';
 
 interface SearchBarProps {
-    id: string
+    id: string,
+    clickEvent:any,
+    ref:React.RefObject<any>,
 }
 
 const SearchBar = (props: SearchBarProps) => {
     return (
         <div className='flex items-center justify-center h-1/12 font-medium *:h-4/7 *:bg-[#7dcdff] '>
-            <input className='rounded-l-full w-2/6 pl-3 outline-none border-none' id={props.id}  type='text' placeholder={`Enter ${props.id} name...`}/>
-            <button className='rounded-r-full px-2 hover:bg-[#26a6eb]'><IoSearch/></button>
+            <input className='rounded-l-full w-2/6 pl-3 outline-none border-none' id={props.id}  type='text' placeholder={`Enter ${props.id} name...`} onKeyDown={(e)=>{
+                if (e.key === 'Enter'){
+                    props.clickEvent();
+                }
+            }} ref={props.ref}/>
+            <button className='rounded-r-full px-2 hover:bg-[#26a6eb]' onClick={props.clickEvent} ><IoSearch/></button>
         </div>
     )
 }
