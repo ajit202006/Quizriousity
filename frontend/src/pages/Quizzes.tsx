65640446aa879b3a82ca0e0c5ae6b31de30d18fe
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-21 13:52:26 +0530

    Added quizlist to display quizzes

--- frontend/src/pages/Quizzes.tsx
+++ frontend/src/pages/Quizzes.tsx
@@ -1,14 +1,16 @@
-import Navbar from "../components/Navbar"
-import SearchBar from "../components/SearchBar"
+import Navbar from '../components/Navbar';
+import SearchBar from '../components/SearchBar';
+import QuizList from '../components/QuizList';
 
 const Quizzes = () => {
   return (
-    <div className="w-full h-full">
-      <Navbar/>
-      <SearchBar id="quiz"/>
+    <div className='w-full h-full flex flex-col'>
+      <Navbar />
+      <SearchBar id='quiz' />
       <hr />
+      <QuizList />
     </div>
   )
 }
 
-export default Quizzes
+export default Quizzes;
