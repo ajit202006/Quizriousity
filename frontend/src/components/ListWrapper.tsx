b2eab03f489d132e0bc9ce2a81f0361162add5a9
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-24 18:14:44 +0530

    Updated css for the scroll bar to make custom scrollbar

--- frontend/src/components/ListWrapper.tsx
+++ frontend/src/components/ListWrapper.tsx
@@ -1,8 +1,8 @@
 const ListWrapper = (props:any) => {
     return (
-        <div className=' h-9/11 my-auto'>
+        <div className={'h-9/11 my-auto ' + props.className}>
             <ul className='h-full p-4 flex flex-col overflow-auto gap-3.5 text-2xl font-semibold
-            [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-[#B9D5FF] [&::-webkit-scrollbar-thumb]:bg-[#0674A0] [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:rounded-full 
+            custom-scrollbar 
             *:flex *:items-center *:min-h-1/8 *:bg-[#39686F] *:rounded-xl *:px-7 '>
                 { props.children }
             </ul>
