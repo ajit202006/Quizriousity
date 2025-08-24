a5f1389386b6f67fab6d5a4364c9ae0efef70aa4
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-24 19:15:56 +0530

    Improved functionality to searchHandler function

--- frontend/src/pages/Users.tsx
+++ frontend/src/pages/Users.tsx
@@ -5,35 +5,35 @@ import UserList from '../components/UserList';
 import TokenContext from '../contexts/TokenContext';
 
 const serverURL = "http://localhost:3000";
+
 const Users = () => {
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([{}]);
     const nameRef = useRef<HTMLInputElement>(null);
     const tokenContext = useContext(TokenContext);
     const token = tokenContext.token;
-    // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2ODlmMTc3NDhjYTJkNTg4MzUzOTJjODAiLCJpYXQiOjE3NTU3NjkyMzksImV4cCI6MTc1NTc3MjgzOX0.hQJxbRv7PD94A50iANvRU56Bi929_knv-s9ijlgit50"
 
-    // const searchHandler = async () => {
-    //     let userName = nameRef.current;
-    //     if (!userName?.value || userName.value.length < 3) {
-    //         alert("Enter at least 3 characters to search");
-    //     } else {
-    //         const response = await fetch(serverURL + "/user/search", {
-    //             method: "POST",
-    //             body: JSON.stringify({ userName:userName.value }),
-    //             headers: {
-    //                 "Content-Type": "application/json",
-    //                 "Authorization": "Bearer " + token
-    //             }
-    //         });
-    //         const result = await response.json();
-    //         setUsers(result.data);
-    //     }
-    // }
+    const searchHandler = async () => {
+        let userName = nameRef.current;
+        if (!userName?.value || userName.value.length < 3) {
+            alert("Enter at least 3 characters to search");
+        } else {
+            const response = await fetch(serverURL + "/user/search", {
+                method: "POST",
+                body: JSON.stringify({ userName:userName.value }),
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + token
+                }
+            });
+            const result = await response.json();
+            setUsers(result.data);
+        }
+    }
 
     return (
         <div className='w-full h-full flex flex-col'>
             <Navbar />
-            <SearchBar id='user' clickEvent={null} ref={nameRef} />
+            <SearchBar id='user' clickEvent={searchHandler} ref={nameRef} />
             <hr />
             <UserList users={users} />
         </div>
