5594bf1532a35b21f906cb88b9e01c89afb4999a
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-24 18:57:35 +0530

    Added useEffect to fecth data from backend to display personal quizzes

--- frontend/src/pages/MyQuizzes.tsx
+++ frontend/src/pages/MyQuizzes.tsx
@@ -1,24 +1,53 @@
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { HiPencilAlt, HiOutlineTrash } from "react-icons/hi";
+import { HiPencilAlt, HiOutlineTrash } from 'react-icons/hi';
 import Button from '../components/Button';
 import Header from '../components/Header';
+import TokenContext from '../contexts/TokenContext';
+
+interface QuizInterface{
+    _id:string,
+    name:string
+}
+
+const serverURL = 'http://localhost:3000';
 
 const MyQuizzes = () => {
-    const published = [{ _id: "null", name: "---" }];
-    const unpublished = [{ _id: "null", name: "---" }];
-    const [isPublished, setIsPublished] = useState(true);
-    //   const nameRef = useRef<HTMLInputElement>(null);
     const navigate = useNavigate();
+    const tokenContext = useContext(TokenContext);
+    const [published, setPublished] = useState([]);
+    const [unpublished, setUnpublished] = useState([]);
+    const [isPublished, setIsPublished] = useState(true);
 
-    const publishedList = published.map((quiz) => {
+    useEffect(() => {
+        fetch(serverURL + `/user/${tokenContext.userId}/quizzes`, {
+            headers: {
+                'Authorization': 'Bearer ' + tokenContext.token
+            }
+        })
+            .then(response => response.json())
+            .then(result => setPublished(result.data))
+            .catch(err => console.log(err));
+    },[]);
+    useEffect(() => {
+        fetch(serverURL + `/user/${tokenContext.userId}/quizzes/unpublished`, {
+            headers: {
+                'Authorization': 'Bearer ' + tokenContext.token
+            }
+        })
+            .then(response => response.json())
+            .then(result => setUnpublished(result.data))
+            .catch(err => console.log(err));
+    },[]);
+
+    const publishedList = published.map((quiz:QuizInterface) => {
         return (
             <li id={quiz._id} key={quiz._id}>
                 <p className='list-item'>{quiz.name}</p>
             </li>
         )
     })
-    const unpublishedList = unpublished.map((quiz) => {
+    const unpublishedList = unpublished.map((quiz:QuizInterface) => {
         return (
             <li className='flex justify-between' id={quiz._id} key={quiz._id}>
                 <p className='list-item'>{quiz.name}</p>
@@ -39,12 +68,12 @@ const MyQuizzes = () => {
                 <Button value='Quizzes' onClick={() => navigate('/quizzes')} />
             </div>
             <div className='flex justify-around min-h-1/12 *:text-3xl *:w-full'>
-                <button className={isPublished ? "bg-[#4ABFEE]" : "bg-[#889FE5]"} onClick={() => setIsPublished(true)} >Published</button>
-                <button className={isPublished ? "bg-[#889FE5]" : "bg-[#4ABFEE]"} onClick={() => setIsPublished(false)}>Unpublished</button>
+                <button className={isPublished ? 'bg-[#4ABFEE]' : 'bg-[#889FE5]'} onClick={() => setIsPublished(true)} >Published</button>
+                <button className={isPublished ? 'bg-[#889FE5]' : 'bg-[#4ABFEE]'} onClick={() => setIsPublished(false)}>Unpublished</button>
             </div>
-            <div className='h-2/3 '>
+            <div className='h-2/3 bg-[#4ABFEE]'>
                 <ul className='list'>
-                    {myList}
+                    {myList.length ? myList : 'Nothing to display'}
                 </ul>
 
             </div>
