d2ec7583d3b969646ed072a0cb92891cd4a978ac
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-24 18:29:59 +0530

    Added navigation to user dashboard page on evry card in the list

--- frontend/src/components/UserList.tsx
+++ frontend/src/components/UserList.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import ListWrapper from "./ListWrapper";
 
 interface UserInterface {
@@ -9,13 +10,15 @@ interface UserInterface {
     passedCount: number
 }
 
-interface UserListProps{
-    users:Array<any>;
+interface UserListProps {
+    users: Array<any>;
 }
-const UserList = (props:UserListProps) => {
+
+const UserList = (props: UserListProps) => {
+    const navigate = useNavigate();
     const userList = props.users.map((user: UserInterface) => {
         return (
-            <li id={user._id}>
+            <li id={user._id} key={user._id} onClick={() =>navigate(`/users/${user._id}`)}>
                 {user.name}
             </li>
         );
