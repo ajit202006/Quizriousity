bd829c8b14c1bc06f0b59cc52d80d5af277b0456
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-24 18:15:35 +0530

    Updated css of the background

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,21 +1,19 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import logo from '/logo.png'
-
 
 const Navbar = () => {
   return (
-    <div className='flex h-1/12 justify-between bg-[#0674A0] w-screen '>
+    <div className='flex h-1/12 justify-between bg-header w-screen '>
       <div className='flex gap-2 items-center p-2'>
         <img src={logo} alt="logo" width={50} height={50} />
         <p className='text-4xl font-semibold'>Quizriousity</p>
       </div>
-      <div className='flex h-full items-end *:grid *:place-items-center w-3/5 justify-center gap-6 *:h-3/4 *:w-1/6 *:rounded-t-2xl *:bg-[#005678] *:text-2xl *:font-semibold'>
-        <Link to="/quizzes">Quizzes</Link>
-        <Link to="/reports">Reports</Link>
-        <Link to="/users">Users</Link>
-        <Link to="/dashboard">Dashboard</Link>
+      <div className='flex h-full items-end *:grid *:place-items-center w-3/5 justify-center gap-6 *:h-3/4 *:w-1/6 *:rounded-t-2xl *:bg-button *:text-2xl *:font-semibold'>
+        <NavLink to="/quizzes">Quizzes</NavLink>
+        <NavLink to="/reports">Reports</NavLink>
+        <NavLink to="/users">Users</NavLink>
+        <NavLink to="/user" >Dashboard</NavLink>
       </div>
-
     </div>
   )
 }
