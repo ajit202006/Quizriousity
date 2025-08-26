b3af1a0c42214a4c54cbbfa5905eb3af885863da
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-26 20:03:15 +0530

    Created page to add quiz 'not working yet'

---
+++ frontend/src/pages/AddQuiz.tsx
@@ -0,0 +1,94 @@
+import Button from "../components/Button"
+import Header from "../components/Header"
+
+const AddQuiz = () => {
+    return (
+        <div className="w-full h-full flex flex-col">
+            <Header />
+            <section id='quiz-view' className='min-w-full min-h-max h-full font-semibold overflow-y-auto custom-scrollbar py-4 flex justify-center '>
+                <div className='w-95/100 min-h-90/100 flex flex-col items-center justify-between p-4 rounded-3xl bg-quiz-view-bg text-black'>
+                    <h1 className='max-w-4/5 text-4xl' >Create your quiz</h1>
+                    <ul className='w-4/5 text-xl py-5 flex flex-col items-center '>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                        <li>zesxdklm;,</li>
+                    </ul>
+                    <div className='w-4/5 flex font-normal justify-around text-white' >
+                        <Button value='Start Exam' onClick={() => console.log('/')} />
+                        
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default AddQuiz
