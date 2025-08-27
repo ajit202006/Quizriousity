711e1da3697106a7902a1b33b64dd1cf4206e2d0
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-27 17:34:25 +0530

    Changed bg color for list items

--- frontend/src/components/ListWrapper.tsx
+++ frontend/src/components/ListWrapper.tsx
@@ -3,12 +3,11 @@ const ListWrapper = (props:any) => {
         <div className={'h-9/11 my-auto ' + props.className}>
             <ul className='h-full p-4 flex flex-col overflow-auto gap-3.5 text-2xl font-semibold
             custom-scrollbar 
-            *:flex *:items-center *:min-h-1/8 *:bg-[#39686F] *:rounded-xl *:px-7 '>
+            *:flex *:items-center *:min-h-1/8 *:bg-li *:rounded-xl *:px-7 '>
                 { props.children }
             </ul>
         </div>
     )
 }
 
-export default ListWrapper
-
+export default ListWrapper;
