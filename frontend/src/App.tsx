771cc6735991f17969cfa1d54f98f9d9fd306beb
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-27 17:31:56 +0530

    Added route to reviews in application

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,27 +12,28 @@ import UserDashboard from './pages/UserDashboard';
 import MyQuizzes from './pages/MyQuizzes';
 import QuizView from './pages/QuizView';
 import AddQuiz from './pages/AddQuiz';
-
+import Review from './pages/Review';
 
 function App() {
   return (
-      <div className='h-screen w-screen flex items-center justify-center bg-background text-white'>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/quizzes' element={<Quizzes />} />
-          <Route path='/reports' element={<Reports />} />
-          <Route path='/users' element={<Users />} />
-          <Route path='/user' element={<Dashboard />} />
-          <Route path='/user/update_password' element={<UpdatePassword />} />
-          <Route path='/user/:userId/myquizzes' element={<MyQuizzes />} />
-          <Route path='/users/:userId/' element={<UserDashboard />} />
-          <Route path='/users/:userId/quiz/:quizId' element={<QuizView />} />
-          <Route path='/quiz' element={<AddQuiz />} />
-        </Routes>
-      </div>
+    <div className='h-screen w-screen flex items-center justify-center bg-background text-white'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/quizzes' element={<Quizzes />} />
+        <Route path='/reports' element={<Reports />} />
+        <Route path='/users' element={<Users />} />
+        <Route path='/user' element={<Dashboard />} />
+        <Route path='/user/update_password' element={<UpdatePassword />} />
+        <Route path='/user/:userId/myquizzes' element={<MyQuizzes />} />
+        <Route path='/users/:userId/' element={<UserDashboard />} />
+        <Route path='/users/:userId/quiz/:quizId' element={<QuizView />} />
+        <Route path='/quiz' element={<AddQuiz />} />
+        <Route path='/users/:userId/quiz/:quizId/reviews/:quizName' element={<Review />} />
+      </Routes>
+    </div>
   )
 }
 
-export default App
+export default App;
