322f337d796c86db1414af376477f578d332c8b0
pushpanjali kumari <ajitvishwakarma202006@gmail.com>
2025-08-30 18:32:38 +0530

    Updated working of back button

--- frontend/src/components/BackButton.tsx
+++ frontend/src/components/BackButton.tsx
@@ -1,15 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 import { IoIosArrowRoundBack } from "react-icons/io";
 
-interface BackButtonProps{
-    toPath:string,
-    title:string
-}
-
-const BackButton = (props:BackButtonProps) => {
+const BackButton = () => {
     const navigate = useNavigate();
     return (
-        <button className='absolute grid left-10 top-10 w-[3vw] h-[3vw] text-4xl rounded-full bg-button place-items-center' onClick={() => navigate(props.toPath)} title={props.title}><IoIosArrowRoundBack /></button>
+        <button className='absolute grid left-10 top-10 w-[3vw] h-[3vw] text-4xl rounded-full bg-button place-items-center' onClick={() => navigate(-1)} ><IoIosArrowRoundBack /></button>
     )
 }
 
